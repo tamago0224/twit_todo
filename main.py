944a0db7d8a49ce0f8a7724db9ae7dd384ceec6d
tamago <bigtreesasuke200733@gmail.com>
2018-06-03 15:41:47 +0900

    surveillance user tweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from twitter import Api
 import yaml
+import tweepy
 
 def get_api_key(yaml_file):
     with open(yaml_file) as file:
@@ -7,20 +7,35 @@ def get_api_key(yaml_file):
 
     return yml_data
 
-def tweet(api, message):
-    api.PostUpdate(message) 
+data = get_api_key("tw_config.yml")
+COMSUMER_KEY = data['comsumer_key']
+COMSUMER_SECRET = data['comsumer_secret']
+ACCESS_TOKEN_KEY = data['access_token']
+ACCESS_TOKEN_SECRET = data['access_token_secret']
 
-def main():
-    data = get_api_key("tw_config.yml")
-    api = Api(data['comsumer_key'],
-            data['comsumer_secret'],
-            data['access_token'],
-            data['access_token_secret'])
+TARGET_HASHTAG = data['target_hashtag']
 
-    statuses = api.GetUserTimeline(api.VerifyCredentials().id)
-    print([s.text for s in statuses])
-    #tweet(api, 'This is Test Tweet.')
+users = ['tamago_0224']
+
+auth = tweepy.OAuthHandler(COMSUMER_KEY, COMSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 
 
-if __name__ == '__main__':
-    main()
+class MyTweetStream(tweepy.StreamListener):
+    def on_status(self, status):
+        if status.user.screen_name in users:
+            print('-'*20)
+            print(status.text.split())
+        return True
+
+    def on_err(self, status_code):
+        print('Got an error with statsu code: '+ str(status.code))
+        return True
+
+    def on_timeout(self):
+        print('Timeout...')
+        return True
+
+myTweetStream = MyTweetStream()
+mystream = tweepy.Stream(auth, myTweetStream)
+mystream.filter(track=[TARGET_HASHTAG])
