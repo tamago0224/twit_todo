c13a185790ea27ce92f52d6ae0ec20ee36169ea0
tamago0224 <bigtreesasuke200733@gmail.com>
2018-06-29 01:00:03 +0900

    temp

--- wunderlist.py
+++ wunderlist.py
@@ -11,9 +11,6 @@ def get_wunderlist_api_keys(yaml_file):
 
 
 def setup():
-    data = get_wunderlist_api_keys("tw_config.yml")
-    CLIENT_ID = data['client_id']
-    ACCESS_TOKEN = data['access_token']
     oauth_url = 'https://a.wunderlist.com/api/v1/user'
     params = {}
 
@@ -26,8 +23,43 @@ def setup():
         user = json.loads(req.text)
         print(user['name'])
     else:
+        print("Auth Error: %d" % req.status_code)
+
+    return wunderlist
+
+
+def list_task(wunderlist):
+    list_url = 'https://a.wunderlist.com/api/v1/lists'
+    params = {}
+
+    req = wunderlist.get(list_url, params=params)
+
+    if req.status_code == 200:
+        lists = json.load(req.text)
+        for l in lists:
+            print(l['id'], l['title'].encode('utf-8'))
+    else:
         print("Error: %d" % req.status_code)
 
 
+def add_task(wunderlist):
+    add_url = 'https://a.wunderlist.com/api/v1/tasks'
+    params = {
+        "list_id": 1,
+        "title": "テスト",
+    }
+
+    req = wunderlist.post(add_url, json=params)
+
+    if req.status_code == 201:
+        print("task add successfuly")
+    else:
+        print("task add failed: %d" % req.status_code)
+
+
 if __name__ == '__main__':
-    setup()
+    data = get_wunderlist_api_keys("tw_config.yml")
+    CLIENT_ID = data['client_id']
+    ACCESS_TOKEN = data['access_token']
+    wunderlist = setup()
+    add_task(wunderlist)
