73c38a284b4744e61a29b3e230bc8ce204fa8be9
tamago0224 <bigtreesasuke200733@gmail.com>
2018-06-28 23:53:54 +0900

    wunderlist api

--- wunderlist.py
+++ wunderlist.py
@@ -1,7 +1,33 @@
-import requests
+import yaml
+import json
+from requests_oauthlib import OAuth2Session
 
-def get_outlook_api_keys(yaml_file):
+
+def get_wunderlist_api_keys(yaml_file):
     with open(yaml_file) as y:
-        yaml_data = yaml.load(y)
+        yml_data = yaml.load(y)
 
-    return yaml_data['OutlookTask']
+    return yml_data['Wunderlist']
+
+
+def setup():
+    data = get_wunderlist_api_keys("tw_config.yml")
+    CLIENT_ID = data['client_id']
+    ACCESS_TOKEN = data['access_token']
+    oauth_url = 'https://a.wunderlist.com/api/v1/user'
+    params = {}
+
+    wunderlist = OAuth2Session()
+    wunderlist.headers['X-Client-ID'] = CLIENT_ID
+    wunderlist.headers['X-Access-Token'] = ACCESS_TOKEN
+    req = wunderlist.get(oauth_url, params=params)
+
+    if req.status_code == 200:
+        user = json.loads(req.text)
+        print(user['name'])
+    else:
+        print("Error: %d" % req.status_code)
+
+
+if __name__ == '__main__':
+    setup()
