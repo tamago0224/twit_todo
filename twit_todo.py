40d0c2568b6deba975d9d58d6fb0253ad75945c9
tamago0224 <bigtreesasuke200733@gmail.com>
2018-07-08 20:52:55 +0900

    wunderlists api change to class

--- twit_todo.py
+++ twit_todo.py
@@ -1,7 +1,6 @@
 import yaml
-import tweepy
-import json
-from requests_oauthlib import OAuth2Session
+from twython import TwythonStreamer
+from wunderlistAPI import WunderlistAPI
 
 
 def get_twitter_api_keys(yaml_file):
@@ -11,79 +10,15 @@ def get_twitter_api_keys(yaml_file):
     return yml_data['Twitter']
 
 
-def get_wunderlist_api_keys(yaml_file):
-    with open(yaml_file) as y:
-        yml_data = yaml.load(y)
-
-    return yml_data['Wunderlist']
-
-
-def init_wunderlist():
-    wunder_data = get_wunderlist_api_keys("tw_config.yml")
-    client_id = wunder_data['client_id']
-    access_token = wunder_data['access_token']
-    url = 'https://a.wunderlist.com/api/v1/user'
-    params = {}
-
-    wunder = OAuth2Session()
-    wunder.headers['X-Client-ID'] = client_id
-    wunder.headers['X-Access-Token'] = access_token
-    req = wunder.get(url, params=params)
-
-    if req.status_code == 200:
-        return wunder
-    else:
-        return None
-
-
-def show_list(wunder):
-    url = 'https://a.wunderlist.com/api/v1/lists'
-    params = {}
-    req = wunder.get(url, params=params)
-
-    if req.status_code == 200:
-        lists = json.loads(req.text)
-        for l in lists:
-            print("ID: {}, TITLE: {}".format(l['id'], l['title']))
-    else:
-        print("Error: status code: %d, do not get todo list." % req.status_code)
-
-
-def add_task(wunder, title):
-    url = 'https://a.wunderlist.com/api/v1/tasks'
-    params = {
-            "list_id": 355782822,
-            "title": title,
-            }
-
-    req = wunder.post(url, json=params)
-
-    if req.status_code == 200:
-        print("task '%s' add successfuly." % title)
-    else:
-        print("Error: task add failed")
-
-
-class MyTweetStream(tweepy.StreamListener):
-    def __init__(self, users):
+class TwitStreamer(TwythonStreamer):
+    def __init__(self, comsumer_key, comsumer_secret, access_token_key, access_token_secret, users):
+        super().__init__(comsumer_key, comsumer_secret, access_token_key, access_token_secret)
         self.users = users
-        self.wunder = init_wunderlist()
-        pass
+        self.wunderlist = WunderlistAPI()
 
-    def on_status(self, status):
-        if status.user.screen_name in self.users:
-            title = status.text.split()[0]
-            add_task(self.wunder, title)
-
-        return True
-
-    def on_err(self, status_code):
-        print('Got an error with statsu code: ' + str(status_code))
-        return True
-
-    def on_timeout(self):
-        print('Timeout...')
-        return True
+    def on_success(self, data):
+        if data['screen_name'] in self.users:
+            self.wunderlist.add_task(data['text'])
 
 
 def main():
@@ -94,13 +29,10 @@ def main():
     ACCESS_TOKEN_KEY = data['access_token']
     ACCESS_TOKEN_SECRET = data['access_token_secret']
     TARGET_HASHTAG = data['target_hashtag']
-    users = ['tamago_0224']
-    auth = tweepy.OAuthHandler(COMSUMER_KEY, COMSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+    TARGET_USERS = data['target_users']
 
-    myTweetStream = MyTweetStream(users)
-    mystream = tweepy.Stream(auth=auth, listener=myTweetStream)
-    mystream.filter(track=[TARGET_HASHTAG])
+    twitStreamer = (COMSUMER_KEY, COMSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, TARGET_USERS)
+    twitStreamer.statuses.filter(track=TARGET_HASHTAG)
 
 
 if __name__ == '__main__':
